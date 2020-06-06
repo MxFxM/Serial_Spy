a9af99817465ed56fed6645780ad63af39bb59db
MxFxM <mxfxmmueller@gmail.com>
2020-06-06 17:33:24 +0200

    simplest version done

--- Serial_Spy/src/main.cpp
+++ Serial_Spy/src/main.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
 
+#define SPY Serial2
+#define USB Serial
+
 void setup() {
-  // put your setup code here, to run once:
+  USB.begin(9600); // usb is always 12 Mbit/s
+  SPY.begin(9600);
+
+  // wait for connection to pc
+  while (!USB.available()) {
+    delay(1);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (USB.available()) {
+    uint8_t newByte = USB.read();
+    SPY.write(newByte);
+  }
+
+  if (SPY.available()) {
+    uint8_t newByte = SPY.read();
+    USB.write(newByte);
+  }
 }
